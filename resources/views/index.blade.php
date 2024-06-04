db7abc044a67d2ff6631a0556bc779a6f58d2cb1
shahjahan45 <sjeel663@gmail.com>
2024-06-04 12:53:22 +0300

    basic Ui

---
+++ resources/views/index.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.master')
+@section('content')
+
+<div class="main-content">
+    <div class="card">
+        <div class="card-header">
+            All Posts
+            <a class="btn btn-success">Create</a>
+            <a class="btn btn-dark">Trash</a>
+        </div>
+        <div class="card-body">
+            <table class="table table-striped table-hover table-bordered border-dark">
+                <thead style="background-color:azure">
+                  <tr>
+                    <th scope="col" style="width:y%">#</th>
+                    <th scope="col" style="width:10%">image</th>
+                    <th scope="col" style="width:20%">title</th>
+                    <th scope="col" style="width:30%">description</th>
+                    <th scope="col" style="width:10%">category</th>
+                    <th scope="col" style="width:10%">publish date</th>
+                    <th scope="col" style="width:20%">
+                        
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td><img src="https://picsum.photo/200" alt=""></td>
+                    <td>Lorem ipsum dolor sit amet, cons</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis ab odio veritatis quas et provident? Repudiandae repellendus mollitia nisi voluptas maiores nam expedita debitis fugiat. Incidunt itaque velit explicabo pariatur.</td>
+                    <td>News</td>
+                    <td>2-5-22</td>
+                    <td>
+                      <a class="btn-sm btn-success">show</a>
+                      <a class="btn-sm btn-primary">Edit</a>
+                      <a class="btn-sm btn-danger">update</a>
+
+                    </td>
+
+                  </tr>
+                  
+                </tbody>
+              </table>
+        </div>
+    </div>
+    
+
+</div>
+
+
+    
+@endsection
